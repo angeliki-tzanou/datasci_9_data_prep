21ae13e1969ae859535a3234d3a673845a4fd782
angeliki-tzanou <angeliki.tzanou@stonybrook.edu>
2023-11-23 12:12:02 +0000

    computed

--- p3_compute.py
+++ p3_compute.py
@@ -17,25 +17,25 @@ df1_clean = pd.read_csv('datasets/processed/LeadBloodLevels_2017_byMSA.csv')
 X1 = df1_clean.drop('pctunder18ebll', axis=1)
 y1 = df1_clean['pctunder18ebll']
 
-# Initialize the StandardScaler
+# Using the StandardScaler
 scaler = StandardScaler()
 X_scale1 = scaler.fit_transform(X1)
 
-# Split the data into training and testing sets
+# Splitting the data into training and testing sets
 X_train1, X_test1, y_train1, y_test1 = train_test_split(X_scaled, y1, test_size=0.2, random_state=42)
 
-# Instantiate and train a dummy regressor
+# Trainning a dummy regressor
 dummy_regressor = DummyRegressor(strategy='mean')
 dummy_regressor.fit(X_train1, y_train1)
 
-# Make predictions on the test set
+# Making predictions on the test set
 y_pred_dummy = dummy_regressor.predict(X_test1)
 
-# Evaluate using a regression metric like Mean Squared Error
+# Evaluating the Mean Squared Error using regression
 mse_dummy1 = mean_squared_error(y_test1, y_pred_dummy)
-print(f'Mean Squared Error (Dummy): {mse_dummy}')
+print(f'Mean Squared Error (Dummy): {mse_dummy1}')
 
-# Instantiate and train an XGBoost regressor
+# Trainning the XGBoost regressor
 xgboost = XGBRegressor()
 xgboost.fit(X_train1, y_train1)
 
@@ -49,7 +49,7 @@ print(f'Mean Squared Error (XGBoost): {mse_xgboost}')
 # Save the XGBoost model
 joblib.dump(xgboost, 'datasets/processed/df1_clean_computed.pkl')
 
-###############################
+#############################################################
 # Loading clean dataset #2:
 df2_clean= pd.read_csv('datasets/processed/Peds_ED_visits.csv')
 df2_clean.columns
@@ -58,26 +58,25 @@ df2_clean.columns
 X = df2_clean.drop('asthmarelateded_visitsper100age0to17', axis=1)
 y = df2_clean['asthmarelateded_visitsper100age0to17']
 
-# Initialize the StandardScaler
+# Using the StandardScaler
 scaler = StandardScaler()
 X_scaled2 = scaler.fit_transform(X)
 
-# Split the data into training and testing sets
+# Splitting the data into training and testing sets
 X_train2, X_test2, y_train2, y_test2 = train_test_split(X_scaled2, y, test_size=0.2, random_state=42)
 
-# Assuming y_train and y_test are continuous (regression task)
-# Instantiate and train a dummy regressor
+# Trainning a dummy regressor
 dummy_regressor = DummyRegressor(strategy='mean')
 dummy_regressor.fit(X_train2, y_train2)
 
-# Make predictions on the test set
+# Making predictions on the test set
 y_pred_dummy = dummy_regressor.predict(X_test2)
 
-# Evaluate using a regression metric like Mean Squared Error
+# Evaluating the Mean Squared Error using regression
 mse_dummy2 = mean_squared_error(y_test2, y_pred_dummy)
 print(f'Mean Squared Error (Dummy): {mse_dummy2}')
 
-# Instantiate and train an XGBoost regressor
+# Trainning the XGBoost regressor
 xgboost = XGBRegressor()
 xgboost.fit(X_train2, y_train2)
 
@@ -88,5 +87,5 @@ y_pred_xgboost = xgboost.predict(X_test2)
 mse_xgboost = mean_squared_error(y_test2, y_pred_xgboost)
 print(f'Mean Squared Error (XGBoost): {mse_xgboost}')
 
-# Save the XGBoost model
+# Saving the XGBoost model
 joblib.dump(xgboost, 'datasets/processed/df2_computed.pkl')
