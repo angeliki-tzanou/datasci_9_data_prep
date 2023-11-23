1a9130f4737a648256704e5b7fb024975b82aea7
angeliki-tzanou <angeliki.tzanou@stonybrook.edu>
2023-11-23 08:01:48 +0000

    p1

--- p1_extract.py
+++ p1_extract.py
@@ -7,11 +7,22 @@ df = pd.read_csv(datalink1)
 df.size
 df.sample(5)
 
-## save as csv to WK9/code/model_dev/data/raw
-df.to_csv('WK9/code/model_dev/data/raw/crime_data.csv', index=False)
+## save as csv within the data folder under raw
+df.to_csv('datasets/raw/LeadBloodLevels_2017_byMSA.csv', index=False)
 
-## save as pickle to WK9/code/model_dev/data/raw
-df.to_pickle('WK9/code/model_dev/data/raw/crime_data.pkl')
+## save as pickle within the data folder under raw
+df.to_pickle('datasets/raw/LeadBloodLevels_2017_byMSA.pkl')
 
 ####################################
-# Dataset 2: Peds ED and PC visits
+# Dataset 2: Peds ED visits
+
+datalink2 = 'https://data.wprdc.org/dataset/6ff17545-2e53-4e51-a9e4-0c4681ad3e4b/resource/17c21e89-ad01-47f5-a8b2-29bdd493666b/download/r3_ed_opendata.csv'
+df = pd.read_csv(datalink2)
+df.size
+df.sample(5)
+
+## save as csv within the data folder under raw
+df.to_csv('datasets/raw/Peds_ED_visits.csv', index=False)
+
+## save as pickle within the data folder under raw
+df.to_pickle('datasets/raw/Peds_ED_visits.pkl')
